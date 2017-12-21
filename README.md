9239075303038987d77af0411562b85256e8d6c5
Eric Ziegler <cerna_gora@me.com>
2017-12-20 17:03:38 -0700

    logging, arg parsing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Image Database Builder
 Creates a database of all photos found under a given parent directory. I was inspired by Picasa, but wanted something a little more light weight.
+
+## Todo
+- collect metadata (exif, xmp, iptc?)
---
+++ builder.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import errno
+from pathlib import Path
+
+
+# photos stuff
+# collect parent directory from user
+# gather all folders and sub folders
+# -- each folder becomes an album(choose name?)
+# gather all photos, grouped by folder
+
+# database stuff
+# create new database
+# -- what if it already exists?
+# create photos table
+# create records
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', help='Parent directory to scan and create database from.')
+    return parser.parse_args()
+
+
+# todo rename me
+def make_sure_path_exists(path):
+    try:
+        Path(path).mkdir()
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+
+def get_logger():
+    new_logger = logging.getLogger('database_builder')
+    new_logger.setLevel(logging.DEBUG)
+    make_sure_path_exists('logs')
+
+    file_handler = logging.FileHandler('logs/database_builder.log')
+    file_handler.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+
+    new_logger.addHandler(file_handler)
+    new_logger.addHandler(stream_handler)
+    return new_logger
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    logger = get_logger()
+
+    logger.info('Collecting photos and subdirectories of \'{}\'.'.format(args.directory))
+
+    parent_path = Path(args.directory)
+    if parent_path.is_dir():
+        logger.info('cool')
+    else:
+        logger.critical('Not a directory: {}'.format(parent_path.name))
