1cda3820958d01cf2adebd9948c7ba9a776004bb
Eric Ziegler <eric.donald.ziegler@gmail.com>
2019-03-22 22:51:37 -0600

    add docstring to config parsing

--- src/config.py
+++ src/config.py
@@ -3,12 +3,21 @@ from pathlib import Path
 
 config = configparser.ConfigParser()
 
-config_path = Path('config.ini')
-if config_path.exists():
-    config.read(config_path)
-else:
-    config['database'] = {'location': 'image-database.db'}
-    config['images'] = {'extensions': '.jpeg,.jpg,.png,.gif'}
-    with open(config_path, 'w') as configfile:
-        config.write(configfile)
-    config.read(config_path)
+
+def parse_config():
+    """
+    Parses the config.ini file. If the file is not present, it will
+    programmatically generate one with sensible defaults.
+    """
+    config_path = Path('config.ini')
+    if config_path.exists():
+        config.read(config_path)
+    else:
+        config['database'] = {'location': 'image-database.db'}
+        config['images'] = {'extensions': '.jpeg,.jpg,.png,.gif'}
+        with open(config_path, 'w') as configfile:
+            config.write(configfile)
+        config.read(config_path)
+
+
+parse_config()
